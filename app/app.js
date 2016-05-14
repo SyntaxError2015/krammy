8c4cc2e0ee0a9ed65fe5d0f892a991cffd9fc991
coddo <codobanclaudiu@gmail.com>
2016-05-14 15:00:37 +0300

    Fixed the area where the code will be written to properly take all the characters as wanted

--- app/app.js
+++ app/app.js
@@ -8,11 +8,14 @@ var mainWindow = null;
 app.on('ready', function() {    
     mainWindow = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        center: true,
+        darkTheme: true
     });
     mainWindow.on('closed', function() {
         mainWindow = null;
     });
     Menu.setApplicationMenu(null);
     mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.maximize();
 });
--- app/index.js
+++ app/index.js
@@ -5,11 +5,8 @@ try {
     window.$ = window.jQuery = require('jquery');
     converter = require('./js/converter.js')
 } catch (error) {
-    
+
 }
-
-
-
 
 $(document).ready(function() {
     $("#but").click(function() {
@@ -54,83 +51,82 @@ $(document).ready(function() {
             dragging = false;
         }
     });
-    
-    
-    
+
+
+
 });
 
-
-function textEdited(obj){
-    
-    $('#html-generated').text(converter.convertToHtml($(obj).text()));
+function textEdited(obj) {
+    var htmlCode = converter.convertToHtml($(obj).val());
+    $('#html-generated').text(htmlCode);
+    $('#html-rendered').html(htmlCode);
 }
 
+function resizeBarMouseDown(e, obj) {
+    e.preventDefault();
 
-function resizeBarMouseDown(e, obj){
-        e.preventDefault();
-        
-        var bar = $(obj);
-        var sidebar = $(obj).parent();
-        var container = sidebar.parent();
-        var main = sidebar.next();
-        
-        var isVertical = bar.attr('class').indexOf('-vertical')>0;
-        
-        var ghostbar = null;
-        if(isVertical){
-            ghostbar = $('<div>', {
-                id: 'ghostbar',
-                css: {
-                    height: bar.height(),
-                    top: 0,
-                    left: sidebar.width() - bar.width(),
-                    width: bar.width(),
-                    cursor: 'col-resize'
-                }
-            }).appendTo(container);
-        }
-        else{
-            ghostbar = $('<div>', {
-                id: 'ghostbar',
-                css: {
-                    height: bar.height(),
-                    top: sidebar.height() - bar.height(),
-                    left:0,
-                    width: bar.width(),
-                    cursor: 'row-resize'
-                }
-            }).appendTo(container);
-        }
+    var bar = $(obj);
+    var sidebar = $(obj).parent();
+    var container = sidebar.parent();
+    var main = sidebar.next();
 
-        $(document).mousemove(function(e) {
-            if(isVertical)
-                ghostbar.css("left", e.pageX - container.position().left - ghostbar.width()/2);
-            else
-                ghostbar.css("top", e.pageY - container.offset().top - ghostbar.height()/2);
-        });
-        
-        
-        //bind on mouseup
-        $(document).mouseup(function(e) {
-            var percentage = 0;
-            
-            if(isVertical)
-                percentage = ((e.pageX - container.position().left + ghostbar.width()/2) / container.width()) * 100;
-            else 
-                percentage = ((e.pageY - container.offset().top + ghostbar.height()/2) / container.height()) * 100;
-                
-            var mainPercentage = 100 - percentage;
-            
-            if(isVertical){
-                sidebar.css("width", percentage + "%");
-                main.css("width", mainPercentage + "%");
-            } else{
-                sidebar.css("height", percentage + "%");
-                main.css("height", mainPercentage + "%");
+    var isVertical = bar.attr('class').indexOf('-vertical') > 0;
+
+    var ghostbar = null;
+    if (isVertical) {
+        ghostbar = $('<div>', {
+            id: 'ghostbar',
+            css: {
+                height: bar.height(),
+                top: 0,
+                left: sidebar.width() - bar.width(),
+                width: bar.width(),
+                cursor: 'col-resize'
             }
-            $('#ghostbar').remove();
-            $(document).unbind('mousemove');
-            $(document).unbind('mouseup');
-        });
-        
+        }).appendTo(container);
     }
+    else {
+        ghostbar = $('<div>', {
+            id: 'ghostbar',
+            css: {
+                height: bar.height(),
+                top: sidebar.height() - bar.height(),
+                left: 0,
+                width: bar.width(),
+                cursor: 'row-resize'
+            }
+        }).appendTo(container);
+    }
+
+    $(document).mousemove(function(e) {
+        if (isVertical)
+            ghostbar.css("left", e.pageX - container.position().left - ghostbar.width() / 2);
+        else
+            ghostbar.css("top", e.pageY - container.offset().top - ghostbar.height() / 2);
+    });
+
+
+    //bind on mouseup
+    $(document).mouseup(function(e) {
+        var percentage = 0;
+
+        if (isVertical)
+            percentage = ((e.pageX - container.position().left + ghostbar.width() / 2) / container.width()) * 100;
+        else
+            percentage = ((e.pageY - container.offset().top + ghostbar.height() / 2) / container.height()) * 100;
+
+        var mainPercentage = 100 - percentage;
+
+        if (isVertical) {
+            sidebar.css("width", percentage + "%");
+            main.css("width", mainPercentage + "%");
+        } else {
+            sidebar.css("height", percentage + "%");
+            main.css("height", mainPercentage + "%");
+        }
+        $('#ghostbar').remove();
+        $(document).unbind('mousemove');
+        $(document).unbind('mouseup');
+    });
+
+}
