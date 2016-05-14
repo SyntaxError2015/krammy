2b1de8d39dd2e8cf862d0ca14814426c9743c8fe
bneagu <neagubogdanioan@yahoo.com>
2016-05-14 11:34:40 +0300

    Initial containers test.

--- app/index.js
+++ app/index.js
@@ -21,3 +21,43 @@ function escapeHtml(unsafe) {
          .replace(/"/g, "&quot;")
          .replace(/'/g, "&#039;");
  }
+
+
+
+var i = 0;
+var dragging = false;
+   $('#dragbar').mousedown(function(e){
+       e.preventDefault();
+       
+       dragging = true;
+       var main = $('#main');
+       var ghostbar = $('<div>',
+                        {id:'ghostbar',
+                         css: {
+                                height: main.outerHeight(),
+                                top: main.offset().top,
+                                left: main.offset().left
+                               }
+                        }).appendTo('body');
+       
+        $(document).mousemove(function(e){
+          ghostbar.css("left",e.pageX+2);
+       });
+       
+    });
+
+   $(document).mouseup(function(e){
+       if (dragging) 
+       {
+           var percentage = (e.pageX / window.innerWidth) * 100;
+           var mainPercentage = 100-percentage;
+           
+           $('#console').text("side:" + percentage + " main:" + mainPercentage);
+           
+           $('#sidebar').css("width",percentage + "%");
+           $('#main').css("width",mainPercentage + "%");
+           $('#ghostbar').remove();
+           $(document).unbind('mousemove');
+           dragging = false;
+       }
+    });
